129c8d2f63350e836b16be61f1f3980ab7c022cc
0LuKi <lukassellemond07@gmail.com>
2025-10-14 23:45:25 +0200

    basic structure of homepage

---
+++ lib/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF326FBB);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onPrimaryAccent = Color.fromARGB(255, 245, 245, 245);
+  static const Color onPrimaryAccent2 = Color.fromARGB(255, 132, 132, 132);
+}
